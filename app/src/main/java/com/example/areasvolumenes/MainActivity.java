60d7932b9c77a1477f4bb62a1a34d6b9954e5ad3
Jose Bandera <jbandera@Defensoria.gov.co>
2020-05-31 21:05:56 -0500

    interfaz OPCIONES lista

--- app/src/main/java/com/example/areasvolumenes/MainActivity.java
+++ app/src/main/java/com/example/areasvolumenes/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.areasvolumenes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView lista_opciones, lista_area, lista_volumenes;
+    private String[] opciones, area, volumenes;
+    private ArrayAdapter<String> adapter, adapter_area;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lista_opciones=findViewById(R.id.lstOpciones);
+        opciones=getResources().getStringArray(R.array.opciones);
+        area=getResources().getStringArray(R.array.areas);
+        volumenes=getResources().getStringArray(R.array.volumenes);
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, opciones);
+        lista_opciones.setAdapter(adapter);
+
+       /* adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, area);
+        area.setAdapter(adapter);
+
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, volumenes);
+        volumenes.setAdapter(adapter);*/
+
+
     }
 }
