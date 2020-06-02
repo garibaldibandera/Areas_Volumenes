38d94e769b311ada5dd6f9aacc5f7cf18746acf9
Jose Bandera <jbandera@Defensoria.gov.co>
2020-06-02 01:43:52 -0500

    Interfaces de Volumenes terminadas

--- app/src/main/java/com/example/areasvolumenes/CalcularVolumenes.java
+++ app/src/main/java/com/example/areasvolumenes/CalcularVolumenes.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,9 +19,34 @@ public class CalcularVolumenes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calcular_volumenes);
+
         lista_volumenes=findViewById(R.id.lstVolumenes);
         volumenes=getResources().getStringArray(R.array.volumenes);
         adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, volumenes);
         lista_volumenes.setAdapter(adapter);
+
+        lista_volumenes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        intent=new Intent(CalcularVolumenes.this,Esfera.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        intent=new Intent(CalcularVolumenes.this,Cilindro.class);
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        intent=new Intent(CalcularVolumenes.this,Cono.class);
+                        startActivity(intent);
+                        break;
+                    case 3:
+                        intent=new Intent(CalcularVolumenes.this,Cubo.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
     }
 }
