56a78a9e7b9abaa41a640a9b6c2eea97081c8d03
Jose Bandera <jbandera@Defensoria.gov.co>
2020-06-03 17:47:32 -0500

    Generar listado ok, falta bilinguismo

--- app/src/main/java/com/example/areasvolumenes/Cono.java
+++ app/src/main/java/com/example/areasvolumenes/Cono.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Cono extends AppCompatActivity {
     private EditText radio, altura;
@@ -24,12 +25,21 @@ public class Cono extends AppCompatActivity {
 
     public void calcular (View v){
         double vlrradio, vlraltura, vlrvolumen=0, pi = 3.1416;
+        String operacion, datos, total;
+        Operaciones operaciones;
         if (validar()){
             vlrradio=Double.parseDouble(radio.getText().toString());
             vlraltura=Double.parseDouble(altura.getText().toString());
             vlrvolumen= (pi*((vlrradio*vlrradio)*vlraltura))/3;
+            resultado.setText(String.format("%1s %.2f",getResources().getString(R.string.valor_volumen),vlrvolumen));
+            datos=getString(R.string.radio)+radio.getText().toString()+getString(R.string.salto_linea)+getString(R.string.altura)+altura.getText().toString();
+            operacion=getResources().getString(R.string.valor_volumen)+" "+getString(R.string.cono);
+            datos=getString(R.string.radio)+radio.getText().toString();
+            total=resultado.getText().toString();
+            operaciones=new Operaciones(operacion, datos, total);
+            operaciones.guardar();
+            Toast.makeText(this,getString(R.string.operacion_guardada), Toast.LENGTH_LONG).show();
         }
-        resultado.setText(String.format("%1s %.2f",getResources().getString(R.string.valor_volumen),vlrvolumen));
     }
     public boolean validar() {
         String error_valor;
